d77903a8070494ca631d9e2534ace6fb24cd565a
MikeH <matroxmike@icloud.com>
2016-11-08 00:07:20 +1100

    format by indent and gitignore

--- file2.c
+++ file2.c
@@ -1,13 +1,10 @@
 // Mon Feb 16 23:37:17 EST 2015 
 
-int max ( int a, int b ) 
-
+int max(int a, int b)
 {
-	if ( a > b ) 
-		return ( a );
-		else
-		return ( b ) ;
-		
-		}
-		
-		
+	if (a > b)
+		return (a);
+	else
+		return (b);
+
+}
--- filex.c
+++ filex.c
@@ -6,47 +6,42 @@
 
 #include <stdio.h>
 
-int a=1;
-int b=2;
+int a = 1;
+int b = 2;
 
-extern int max( int a, int b );
+extern int max(int a, int b);
 
-int main ()
+int main()
 {
 	int c;
 	int d;
-	
+
 	extern int u;
-	
+
 	static int v;
-	
+
 	int w = ONE, x = TWO, y = THREE;
-	
-	int z = 0 ; 
-	
-	z = max ( x, y ) ;
-	w = max ( z, w ) ;
-	
-	printf("%d  %d  %d \n", z , w , u ) ; 
-	
-	}
-	
-	
+
+	int z = 0;
+
+	z = max(x, y);
+	w = max(z, w);
+
+	printf("%d  %d  %d \n", z, w, u);
+
+}
+
 // Mon Feb 16 23:37:17 EST 2015 
 
-int max ( int a, int b ) 
-
+int max(int a, int b)
 {
-	if ( a > b ) 
-		return ( a );
-		else
-		return ( b ) ;
-		
-		}
-		
-		
+	if (a > b)
+		return (a);
+	else
+		return (b);
+
+}
+
 // Mon Feb 16 23:48:36 EST 2015 
 
-int u = 6 ;
-
-
+int u = 6;
